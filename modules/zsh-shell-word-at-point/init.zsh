16a55b69fb470fa1f04731858df93bcfc1e4fd59
zhijia,.zhang <jiahut@gmail.com>
2019-03-08 15:53:37 +0800

    remove zsh-shell-word-at-point

--- modules/zsh-shell-word-at-point/init.zsh
+++
@@ -1,9 +0,0 @@
-
-### init
-source "${0:h}/shell-word-at-point"
-
-autoload -Uz shell-word-at-point
-
-bindkey '^X^F' edit-file-at-point
-bindkey '^X^R' view-file-at-point
-bindkey '^[!' run-shell-word-at-point
